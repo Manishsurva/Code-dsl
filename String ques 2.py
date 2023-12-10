f436213742b04ea70a77aa7092b0e6c139fe1115
athrav908 <153298397+athrav908@users.noreply.github.com>
2023-12-11 02:13:50 +0530

    String ques 2.py

--- String ques 2.py
+++ String ques 2.py
@@ -1,34 +1,42 @@
-string = input("enter your string:")
+string = input("enter the string:")
 wordis = string.split()
-longest_word = max(wordis, key = len)
-print("longest word is :",longest_word)
+longest_word = max(wordis,key=len)
+print("longest word is:",longest_word)
 
-char = input("please enter frequency of character:")
-count = 0
+
+
+char = input("enter the char for fre:")
+count=0
 i=0
 while(i<len(string)):
     if(string[i]==char):
-        count=count+1
+        count=i+1
     i=i+1
-print("freq of the",char,"in stri string is:",count)
+print("fre is :",count)
+
 
 def palindrome(string):
     string = string.lower().replace('','')
-    return string == string[::-1]
+    return string ==string[::-1]
 print("is palindrome:",palindrome(string))
 
-substring = input("enter the sub string:")
-print("index of your sub string is:",string.index(substring))
+substring=input("enter ur substring:")
+print("index of substring is:",string.index(substring))
+
 
 def word_count(str):
-    count = dict()
+    counts = dict()
     words = str.split()
     for word in words:
-        if word in count:
-            count[word] += 1
+        if word in counts:
+            counts[word] +=1
         else:
-            count[word] = 1
-    return
-print("count the occurance of each word is:\n", word_count(string))
+            counts[word]=1
+    return counts
+print("ur word count:\n",word_count(string))
+    
 
 
+    
+               
+               
